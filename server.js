58ed76776d06701d694ac7a72183abfb155707c5
Nelson Rodriguez <nelsao101@gmail.com>
2015-04-29 16:07:44 +0200

    Added server.js

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ var express =require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 No Found');
+});
+
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 Sever Error');
+});
+
 app.get('/', function(req, res, next){
-	console.log('express is working');
-	res.send('Express Server');
+	console.log('express is working with supervisor');
+	res.send('Express Server is configured');
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express is running on http://localhost:'+ app.get('port')+ 'press Ctrl-C to terminate.'
+	console.log('Express is running on http://localhost:'+ app.get('port')+ ' press Ctrl-C to terminate.'
 );
 });
